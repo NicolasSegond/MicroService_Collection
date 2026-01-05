c328eede93d230b41e9a68267ea0bfd8b67acd8b
Nicolas <nicolassegond0@gmail.com>
2026-01-05 23:36:06 +0100

    fix: refactor eslint configuration to improve structure and add ignores for dist and coverage

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,17 +2,14 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import {defineConfig, globalIgnores} from 'eslint/config'
 
-export default defineConfig([
-    globalIgnores(['dist']),
+export default [
+    {
+        ignores: ['dist', 'coverage']
+    },
+    js.configs.recommended,
     {
         files: ['**/*.{js,jsx}'],
-        extends: [
-            js.configs.recommended,
-            reactHooks.configs.flat.recommended,
-            reactRefresh.configs.vite,
-        ],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
@@ -22,7 +19,13 @@ export default defineConfig([
                 sourceType: 'module',
             },
         },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
         rules: {
+            ...reactHooks.configs.recommended.rules,
+
             'no-unused-vars': ['error', {varsIgnorePattern: '^[A-Z_]'}],
             'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
         },
@@ -42,4 +45,4 @@ export default defineConfig([
             },
         },
     },
-])
+]
